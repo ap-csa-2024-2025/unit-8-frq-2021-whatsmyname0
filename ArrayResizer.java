d5597ed73c5b9b3f20fe69a00e2f7b32414f4b01
whatsmyname0 <mikepham2007@gmail.com>
2025-03-21 17:16:36 +0000

    e

--- ArrayResizer.java
+++ ArrayResizer.java
@@ -6,8 +6,17 @@ public class ArrayResizer
    */
   public static boolean isNonZeroRow(int[][] array2D, int r)
   {
-    /* to be implemented in part (a) */
-    return false;
+    for (int row = 0; row < array2D.length; row++)
+    {
+      for (int col = 0; col < array2D[row].length; col++)
+      {
+        if (array2D[r][col] == 0)
+        {
+          return false;
+        }
+      }
+    }
+    return true;
   }
 
   /** Returns the number of rows of array2D that contain all nonzero values.
@@ -31,7 +40,19 @@ public class ArrayResizer
    */
   public static int[][] resize(int[][] array2D)
   {
-    /* to be implemented in part (b) */
-    return null;
+    int [][] ans = new int [numNonZeroRow(array2D)][array2D[0].length];
+    int row = 0;
+    for (int i = 0; i < array2D.length; i++)
+    {
+      if (isNonZeroRow(array2D, i) == true)
+      {
+        for (int j = 0; j < array2D[0].length; j++)
+        {
+          ans[row][j] = array2D[i][j];
+        }
+        row++;
+      }
+    }
+    return ans;
   }
 }
